0fb78093a298c990c35b59e11088fc4ff9215b16
Chris Billington <chrisjbillington@gmail.com>
2017-07-30 17:02:59 -0400

    Add testing_utils module, which provides a TestCase class appropriate for running tests on a GUI program running in the main thread, with the tests running in a separate thread.

---
+++ __init__.py
@@ -0,0 +1,44 @@
+#####################################################################
+#                                                                   #
+# __init__.py                                                       #
+#                                                                   #
+# Copyright 2013, Monash University                                 #
+#                                                                   #
+# This file is part of the labscript suite (see                     #
+# http://labscriptsuite.org) and is licensed under the Simplified   #
+# BSD License. See the license.txt file in the root of the project  #
+# for the full license.                                             #
+#                                                                   #
+#####################################################################
+
+__version__ = '2.3.0-prerelease'
+
+
+import sys
+import os
+
+for path in sys.path:
+    if os.path.exists(os.path.join(path, '.is_labscript_suite_install_dir')):
+        labscript_suite_install_dir = path
+        break
+else:
+    labscript_suite_install_dir = None
+
+
+class VersionException(Exception):
+    pass
+
+
+def check_version(module_name, at_least, less_than, version=None):
+
+    from distutils.version import LooseVersion
+
+    if version is None:
+        version = __import__(module_name).__version__
+    if version is None:
+        raise ValueError('Invalid version string from package {}: {}'.format(module_name, version))
+    at_least_version, less_than_version, installed_version = [LooseVersion(v) for v in [at_least, less_than, version]]
+    if not at_least_version <= installed_version < less_than_version:
+        raise VersionException(
+            '{module_name} {version} found. {at_least} <= {module_name} < {less_than} required.'.format(**locals()))
+
---
+++ setup_logging.py
@@ -0,0 +1,50 @@
+#####################################################################
+#                                                                   #
+# /setup_logging.py                                                 #
+#                                                                   #
+# Copyright 2013, Monash University                                 #
+#                                                                   #
+# This file is part of labscript_utils, in the labscript suite      #
+# (see http://labscriptsuite.org), and is licensed under the        #
+# Simplified BSD License. See the license.txt file in the root of   #
+# the project for the full license.                                 #
+#                                                                   #
+#####################################################################
+
+import sys, os
+import logging, logging.handlers
+import __main__
+
+
+def setup_logging(program_name, log_level=logging.DEBUG, terminal_level=logging.INFO, maxBytes=1024*1024*50, backupCount=1):
+    logger = logging.getLogger(program_name)
+    # Clear any previously added handlers from the logger:
+    for handler in logger.handlers[:]:
+        logger.removeHandler(handler)
+    try:
+        try:
+            program_module = __import__(program_name)
+        except ImportError:
+            program_module = __import__(program_name.lower())
+        main_path = program_module.__file__
+    except ImportError:
+        main_path = __main__.__file__ if hasattr(__main__, '__file__') else __file__
+
+    log_dir = os.path.dirname(os.path.realpath(main_path))
+    log_path = os.path.join(log_dir, '%s.log' % program_name)
+    handler = logging.handlers.RotatingFileHandler(log_path, maxBytes=maxBytes, backupCount=backupCount)
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s: %(message)s')
+    handler.setFormatter(formatter)
+    handler.setLevel(log_level)
+    logger.addHandler(handler)
+    if sys.stdout.isatty():
+        terminalhandler = logging.StreamHandler(sys.stdout)
+        terminalhandler.setFormatter(formatter)
+        terminalhandler.setLevel(terminal_level)
+        logger.addHandler(terminalhandler)
+    else:
+        # Prevent bug on windows where writing to stdout without a command
+        # window causes a crash:
+        sys.stdout = sys.stderr = open(os.devnull, 'w')
+    logger.setLevel(logging.DEBUG)
+    return logger
